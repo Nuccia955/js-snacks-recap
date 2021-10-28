950990da74cb0d176020c2d06f1fcf6d4b77180e
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-28 16:21:52 +0200

    add js-blocco-2 jsnack4

---
+++ js-snacks-blocco-2/jsnack4/main.js
@@ -0,0 +1,18 @@
+/* 
+JSnack 4 
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.*/
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array2 = [1, 2, 3];
+console.log(array1);
+console.log(array2);
+let randomNum;
+while (array2.length < array1.length) {
+    randomNum = Math.floor(Math.random() * 100 + 4);
+    console.log("numero random da aggiungere all'array2", randomNum);
+    array2.push(randomNum);
+    array2.legth++;
+}
+console.log(array2);
